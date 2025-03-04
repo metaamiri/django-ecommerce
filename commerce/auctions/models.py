f46d57376bc63a3e3efab02114a268832915e59b
metaamiri <mohammadamiri007i@gmail.com>
2025-03-04 21:48:37 +0330

    creating models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -9,3 +9,40 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username}"
+    
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    title = models.CharField(max_length=32)
+    description = models.TextField()
+    image_url = models.URLField(blank=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.bid}"
+    
+
+
+class Category(models.Model):
+    listing = models.ManyToManyField(Listing, blank=True, related_name="categories")
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.comment}"
